565dfb170d39b694e1558468ecb3bc42b4c9383c
Ehsanullah <ehsanullahjansharifi@gmail..com>
2025-12-31 20:53:16 +0430

    5commit

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
 const express = require("express");
 const app = express();
 const session = require("express-session");
-const SQLiteStore = require("connect-sqlite3")(session);
+const pgSession = require("connect-pg-simple")(session);
+const db = require("./database/db.js");
+
+app.use(express.static("public"));
+app.engine("html", require("ejs").renderFile);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(
   session({
     store: new pgSession({
       pool: db.pool,
-      tableName: "session",
+      tableName: "user_sessions",
     }),
     secret: process.env.SESSION_SECRET || "your_secret_key",
     resave: false,
-    saveUninitialized: false, // only save sessions when something is stored
+    saveUninitialized: false,
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
+      maxAge: 1000 * 60 * 60 * 24,
     },
   })
+);
 
-
-const port = process.env.PORT || 3001;
-app.use(express.static("public"));
-app.engine("html", require("ejs").renderFile);
 const dashboardRoutes = require("./route/dashbard.js");
 const loginRoutes = require("./route/login.js");
 const signupRoutes = require("./route/signup.js");
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 app.use("/", signupRoutes);
 app.use("/", loginRoutes);
 app.use("/", dashboardRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server running on port ${port}`));
